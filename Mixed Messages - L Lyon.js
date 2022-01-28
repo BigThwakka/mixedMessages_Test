8f46edab624e18c4a0e7d32a140bce854f482cc1
Lloyd <leo10154@hotmail.co.uk>
2022-01-28 01:58:11 +0000

    First commit - Created 2 files

---
+++ Mixed Messages - L Lyon.js
@@ -0,0 +1,20 @@
+/*
+CodeAcademy Portfolio - Mixed Messages
+ Coded by Lloyd; January 26th 2021
+
+ ---- TASK: ----
+    - Message Generator
+    - Run Program > Outputs Random Message
+    - Comprised of three segments in Output
+
+ ---- REQUIREMENTS: ----
+    - Javascript
+    - Git version Control
+    - Command Line
+
+ ---- TOPIC: 'Butchered'Movie Quotes----
+    - 'No, I am your father!'
+    - 'RAAARGH, Get to the Chopper!'
+    - 'Uh say Jarvis, is it that time?'
+*/
+
